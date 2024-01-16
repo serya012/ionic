c4674e465aa32f364fd6bfad5994f39d115381e6
serya012 <ayrescarneiro01@gmail.com>
2024-01-16 00:51:14 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,8 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot({
-      maxEdgeStart: 100, // Ajuste o valor conforme necessário
-    }),
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
